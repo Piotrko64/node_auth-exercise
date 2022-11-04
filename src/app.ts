490906ae8cd358ac6a86a8fdc43ea83195b83715
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-11-04 20:03:34 +0100

    fix problem with create User

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,39 @@
-import bodyParser from 'body-parser'
-import express from 'express'
-import { getPathToView } from './utils/getPathToView'
-import bcrypt from 'bcryptjs'
+import bodyParser from 'body-parser';
+import express from 'express';
+import { getPathToView } from './utils/getPathToView';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
-const app = express()
+const prisma = new PrismaClient();
+const app = express();
 
-app.use(bodyParser.json())
-bodyParser.urlencoded({ extended: true })
+app.use(bodyParser.json());
+bodyParser.urlencoded({ extended: true });
 
 app.get('/register', (req, res) => {
-   res.sendFile(getPathToView('register.html'))
-})
-app.post('/api/user', (req, res) => {
-   let { login, password } = req.body
+   res.sendFile(getPathToView('register.html'));
+});
+
+app.post('/api/user', async (req, res) => {
+   let { login, password } = req.body;
+
    if (!login || !password) {
-      return res.send({ error: 'Something is wrong' })
+      return res.send({ error: 'Something is wrong' });
    }
-   password = bcrypt.hashSync(password, 8)
-   res.json({ login, password })
-})
+
+   password = bcrypt.hashSync(password, 8);
+
+   try {
+      await prisma.user.create({
+         data: { login, password },
+      });
+   } catch (err) {
+      return res.json({ err });
+   }
+
+   res.json(`User ${login} is created`);
+});
 
 app.listen(3100, () => {
-   console.log('App is starting...')
-})
+   console.log('App is starting...');
+});

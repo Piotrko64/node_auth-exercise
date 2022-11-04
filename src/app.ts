7251468da0234876462f151633a206ac8845b5a1
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-11-04 18:41:26 +0100

    fix problem with 🐞 fix: fix problem with get request body

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
+import bodyParser from 'body-parser'
 import express from 'express'
 import { getPathToView } from './utils/getPathToView'
 
 const app = express()
 
+app.use(bodyParser.json())
+bodyParser.urlencoded({ extended: true })
+
 app.get('/', (req, res) => {
    res.sendFile(getPathToView('register.html'))
+})
+app.post('/api/user', (req, res) => {
+   console.log(req.body)
+   res.json(req.body)
 })
 
 app.listen(3100, () => {

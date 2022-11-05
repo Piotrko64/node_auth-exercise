734384d27e4abb4edd5d53b588d3546fdf2c54d6
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-11-05 12:47:02 +0100

    add css styles

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(ejsLayout);
 app.use(router);
+app.use(express.static('./public'));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(`${__dirname}/views/pages`));
